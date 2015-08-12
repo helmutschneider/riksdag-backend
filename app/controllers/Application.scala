d9f46adc6ee8ae8f00441395952c9ba139a6e9b1
Johan Björk <johanimon@gmail.com>
2015-08-12 18:19:18 +0200

    Remove static files

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,26 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
 
 class Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  case class Thing(name: String)
+
+  implicit val thingWrites = new Writes[Thing] {
+    override def writes(o: Thing) = Json.obj(
+      "name" -> o.name
+    )
+  }
+
+  def get = Action {
+
+    val a = Json.toJson(Seq(
+      Thing("Car"),
+      Thing("Boat")
+    ))
+
+    Ok(a.toString())
   }
 
 }
