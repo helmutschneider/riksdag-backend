b84228cb41246b3d9ec089dac85d8e6860ddf5be
JacobNA <jacob.norlinandersson@gmail.com>
2015-08-16 00:27:32 +0200

    Changed id to personId

--- app/remote/Person.scala
+++ app/remote/Person.scala
@@ -39,3 +39,14 @@ case class Person(personId: String, birthYear: Int, gender: Gender,
                   location: String, imageUrl: String) {
 
 }
+
+import sorm._
+
+object  Db extends Instance(
+  entities = Set() +Entity[Person](),
+  url = "jdbc:mysql://theownagecool.se/riksdag_2",
+  user = "romson",
+  password = "vAd3XsVfc3Am37WW577N",
+  initMode = InitMode.Create,
+  poolSize = 1
+)
