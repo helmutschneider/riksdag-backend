cbea91d829e24dd499e633309212f3fd97416f5b
JacobNA <jacob.norlinandersson@gmail.com>
2015-08-18 16:54:56 +0200

    Added json parsin model for documents

---
+++ app/remote/Document.scala
@@ -0,0 +1,28 @@
+package remote
+
+import java.sql.Date
+
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
+
+/**
+ * Created by Jacob on 2015-08-15.
+ */
+
+object Document
+{
+  val jsonReader: Reads[Document] = (
+      (JsPath \ "id").read[String] and
+      (JsPath \ "publicerad").read[Date] and
+      (JsPath \ "titel").read[String] and
+      (JsPath \ "undertitel").read[String] and
+      (JsPath \ "typ").read[String] and
+      (JsPath \ "subtyp").read[String] and
+      (JsPath \ "dokument_url_html").read[String] and
+      (JsPath \ "dokument_url_text").read[String]
+    )(Document.apply _)
+}
+
+
+case class Document(docId: String,  docDate: Date, title: String, subTitle: String, typ: String, subTyp: String, urlHtml: String, urlText: String)
