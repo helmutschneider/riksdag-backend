8ce30c12a864eefa3551e85eb628f11812aa68c5
Johan Björk <johanimon@gmail.com>
2015-08-27 05:23:15 +0200

    Minor scheduling time fixes

--- app/Global.scala
+++ app/Global.scala
@@ -17,29 +17,28 @@ object Global extends GlobalSettings {
   override def onStart(app: Application): Unit = {
     super.onStart(app)
 
-    implicit val currentApp = app
-
     val currentDate = new java.util.Date()
     val runAtHour = 3
 
     val cal = Calendar.getInstance()
-    println(cal.getTime)
-
     cal.set(Calendar.DATE, if (cal.get(Calendar.HOUR_OF_DAY) >= runAtHour) cal.get(Calendar.DATE) + 1 else cal.get(Calendar.DATE))
     cal.set(Calendar.HOUR_OF_DAY, runAtHour)
-    println(cal.getTime.toString)
+    cal.set(Calendar.MINUTE, 0)
+    cal.set(Calendar.SECOND, 0)
 
     val wait = cal.getTime.getTime - currentDate.getTime
 
-    Logger.info(s"Waiting ${wait} ms before syncing next time")
+    Logger.info(s"Scheduling next sync for ${cal.getTime}")
+
+    implicit val currentApp = app
 
     Akka.system.scheduler.schedule(
       Duration.create(wait, TimeUnit.MILLISECONDS),
       Duration.create(1, TimeUnit.DAYS)
     ) {
+      Logger.info("Starting sync")
       val client = new HttpClient
       val mgr = new SyncManager(client)
-      Logger.info("Starting sync")
       mgr.run().map(_ => Logger.info("Sync completed"))
     }
   }
