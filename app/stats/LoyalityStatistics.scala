39f53586cf80dfb39e0115ae1cc41529253b4703
Johan Björk <johanimon@gmail.com>
2015-08-30 03:07:48 +0200

    Fix broken queries

--- app/stats/LoyalityStatistics.scala
+++ app/stats/LoyalityStatistics.scala
@@ -43,9 +43,55 @@ object LoyalVoter {
  */
 class LoyalityStatistics(db: Connection) {
 
+	val votesByResult = """
+			   select
+			   		v.voting_id,
+			   		p.party,
+			   		v.concerns,
+			   		v.result,
+			   		count(*) qty
+			   from vote v
+			   inner join person p
+						on p.person_id = v.person_id
+			   /* remove "absent" votes */
+			   where v.result != ?
+			   group by v.voting_id, p.party, v.concerns, v.result
+  """
+
+	val votesForMostCommon = """
+				select
+				        voting_id,
+				        party,
+				        concerns,
+				        max(qty) most_common_count
+				from (
+				    /* compute the quantity votes (no/yes/abstaining)
+				       grouped by voting id and party */
+				        select
+		                v.voting_id,
+		                p.party,
+		                v.concerns,
+		                count(*) qty
+				        from vote v
+				        inner join person p
+										on p.person_id = v.person_id
+				        inner join voting v2
+										on v2.voting_id = v.voting_id
+				        and v2.sync_id = (
+		                select max(sync_id)
+		                from sync
+		                where completed_at is not null
+				        )
+				        /* remove "absent" votes */
+				        where v.result != ?
+				        group by v.voting_id, p.party, v.concerns, v.result
+				) t2
+				group by t2.voting_id, t2.party, t2.concerns
+  """
+
 	def getDisloyalVotings(personRemoteId: String): List[DisloyalVoting] = {
 
-		val sql = """
+		val sql = s"""
 		        		select
 		        			v1.remote_id,
 		        			d.title,
@@ -54,75 +100,31 @@ class LoyalityStatistics(db: Connection) {
 		        			t4.most_common as party_voted
 		        		from vote v
 		        		inner join voting v1
-		        		on v.voting_id = v1.voting_id
+		        			on v.voting_id = v1.voting_id
 		        		inner join person p
-		        		on p.person_id = v.person_id
+		        			on p.person_id = v.person_id
             		and p.remote_id = ?
 		        		inner join document d
-		        		on d.voting_id = v.voting_id
+		        			on d.voting_id = v.voting_id
 		        		inner join (
-		        			select
-		        				t1.voting_id,
-		        				t1.party,
-		        				t1.result as most_common
-		        			from (
-		        				/* compute the quantity votes (no/yes/abstaining)
-		        				   grouped by voting id and party */
-		        				select
-		        					v.voting_id,
-		        					v.result,
-		        					p.party,
-		        					count(v.result) qty
-		        				from vote v
-		        				inner join person p
-		        				on p.person_id = v.person_id
+										select
+											t1.voting_id,
+											t1.party,
+           						t1.concerns,
+											t1.result as most_common
+										from ( $votesByResult ) t1
 
-		        				/* remove "absent" votes */
-		        				where v.result != ?
-		        				group by v.voting_id, p.party, v.result
-		        			) t1
-
-		        			/* join the quantity of the most common vote so we can determine
-		        			   the most common vote of each party */
-		        			inner join (
-
-		        			    /* compute the quantity of the most common vote
-		        			       grouped by voting id and party */
-		        				select
-		        					voting_id,
-		        					party,
-		        					max(qty) most_common_count
-		        				from (
-
-		        				    /* compute the quantity votes (no/yes/abstaining)
-		        				       grouped by voting id and party */
-		        					select
-		        						v.voting_id,
-		        						p.party,
-		        						count(*) qty
-		        					from vote v
-		        					inner join person p
-		        					on p.person_id = v.person_id
-		        					inner join voting v2
-		        					on v2.voting_id = v.voting_id
-		        					and v2.sync_id = (
-		        						select max(sync_id)
-		        						from sync
-		        						where completed_at is not null
-		        					)
-
-		        					/* remove "absent" votes */
-		        					where v.result != ?
-		        					group by v.voting_id, p.party, v.result
-		        				) t2
-		        				group by t2.voting_id, t2.party
-		        			) t3
-		        			on t3.voting_id = t1.voting_id
-		        			and t3.most_common_count = t1.qty
-		        			and t3.party = t1.party
+										/* join the quantity of the most common vote so we can determine
+											 the most common vote of each party */
+										inner join ( $votesForMostCommon ) t3
+											on t3.voting_id = t1.voting_id
+											and t3.most_common_count = t1.qty
+											and t3.party = t1.party
+											and t3.concerns = t1.concerns
 		        		) t4
 		        		on t4.voting_id = v.voting_id
 		        		and t4.party = p.party
+            		and t4.concerns = v.concerns
 		        		/* remove "absent" votes */
 		        		where v.result != ?
 		        		and v.result != t4.most_common
@@ -154,100 +156,44 @@ class LoyalityStatistics(db: Connection) {
 
   def getLoyality(): List[PersonLoyality] = {
 
-    val sql =
-      """
-        select
-        	p.remote_id,
-        	p.first_name,
-        	p.last_name,
-        	p.party,
-        	t6.disloyal_count
-        from (
-        	/* compute the number of times a person has been disloyal */
-        	select
-        		t5.person_id,
-        		sum(if(t5.is_loyal = 0, 1, 0)) disloyal_count
-        	from (
-        	    /* compare the result of the vote with the party's most common vote
-        	     to determine if the person is loyal */
-
-        		select
-        			v.person_id,
-        			if(v.result = t4.most_common, 1, 0) as is_loyal
-        		from vote v
-        		inner join person p
-        		on p.person_id = v.person_id
-        		inner join (
-        			select
-        				t1.voting_id,
-        				t1.party,
-        				t1.result as most_common
-        			from (
-        				/* compute the quantity votes (no/yes/abstaining)
-        				   grouped by voting id and party */
-        				select
-        					v.voting_id,
-        					v.result,
-        					p.party,
-        					count(v.result) qty
-        				from vote v
-        				inner join person p
-        				on p.person_id = v.person_id
-
-        				/* remove "absent" votes */
-        				where v.result != ?
-        				group by v.voting_id, p.party, v.result
-        			) t1
-
-        			/* join the quantity of the most common vote so we can determine
-        			   the most common vote of each party */
-        			inner join (
-
-        			    /* compute the quantity of the most common vote
-        			       grouped by voting id and party */
-        				select
-        					voting_id,
-        					party,
-        					max(qty) most_common_count
-        				from (
-
-        				    /* compute the quantity votes (no/yes/abstaining)
-        				       grouped by voting id and party */
-        					select
-        						v.voting_id,
-        						p.party,
-        						count(*) qty
-        					from vote v
-        					inner join person p
-        					on p.person_id = v.person_id
-        					inner join voting v2
-        					on v2.voting_id = v.voting_id
-        					and v2.sync_id = (
-        						select max(sync_id)
-        						from sync
-        						where completed_at is not null
-        					)
-
-        					/* remove "absent" votes */
-        					where v.result != ?
-        					group by v.voting_id, p.party, v.result
-        				) t2
-        				group by t2.voting_id, t2.party
-        			) t3
-        			on t3.voting_id = t1.voting_id
-        			and t3.most_common_count = t1.qty
-        			and t3.party = t1.party
-        		) t4
-        		on t4.voting_id = v.voting_id
-        		and t4.party = p.party
-        		/* remove "absent" votes */
-        		where v.result != ?
-        	) t5
-        	group by t5.person_id
-        ) t6
-        inner join person p
-        on p.person_id = t6.person_id
-        order by t6.disloyal_count desc
+    val sql = s"""
+				select
+		       	p.remote_id,
+		        	p.first_name,
+		        	p.last_name,
+		        	p.party,
+		        	t5.disloyal_count
+		      from person p
+		      inner join (
+                select
+                    p.person_id,
+                    sum(if(v.result != t4.most_common_vote, 1, 0)) as disloyal_count
+                from person p
+                inner join vote v
+                on v.person_id = p.person_id
+                inner join (
+                    select
+                        t1.voting_id,
+                        t1.party,
+                        t1.concerns,
+                        t1.result as most_common_vote
+                    from ( $votesByResult ) t1
+                    /* join the quantity of the most common vote so we can determine
+                       the most common vote of each party */
+                    inner join ( $votesForMostCommon ) t3
+												on t3.voting_id = t1.voting_id
+												and t3.most_common_count = t1.qty
+												and t3.party = t1.party
+												and t3.concerns = t1.concerns
+                ) t4
+                on t4.voting_id = v.voting_id
+                and t4.concerns = v.concerns
+                and t4.party = p.party
+                where v.result != ?
+                group by p.person_id
+         	) t5
+         	on p.person_id = t5.person_id
+          order by t5.disloyal_count desc
       """
 
     val stmt = db.prepareStatement(sql)
