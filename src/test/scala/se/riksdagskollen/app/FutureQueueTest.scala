8bc2d4cd014d43c26c2d696512d33e2d6ca10aa0
Johan Björk <johanimon@gmail.com>
2016-05-28 19:10:33 +0200

    Use java concurrent executor instead of Thread.sleep to test FutureQueue

--- src/test/scala/se/riksdagskollen/app/FutureQueueTest.scala
+++ src/test/scala/se/riksdagskollen/app/FutureQueueTest.scala
@@ -1,29 +1,38 @@
 package se.riksdagskollen.app
 
+import java.util.concurrent.{Executors, TimeUnit}
+
 import org.scalatest.FunSuite
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Millis, Seconds, Span}
 
-import scala.concurrent.{Future}
+import scala.concurrent.{Future, Promise}
 
 class FutureQueueTest extends FunSuite with ScalaFutures {
 
   implicit val defaultPatience = PatienceConfig(timeout = Span(5, Seconds), interval = Span(5, Millis))
 
+  val executor = Executors.newScheduledThreadPool(1)
+
   private def makeFunc[T](resolveWith: T, sleep: Int): () => Future[T] = {
     () => {
-      Future {
-        Thread.sleep(sleep)
-        println(s"Resolved with ${resolveWith}")
-        resolveWith
-      }(scala.concurrent.ExecutionContext.global)
+      val promise = Promise[T]
+      executor.schedule(new Runnable {
+        override def run(): Unit = {
+          promise.success(resolveWith)
+        }
+      }, sleep, TimeUnit.MILLISECONDS)
+      promise.future
     }
   }
 
   test("Executes sequentially when concurrency is 1") {
 
     val queue = new FutureQueue[Int](
-      Seq(makeFunc(1, 1000), makeFunc(2, 1000)),
+      Seq(
+        makeFunc(1, 1000),
+        makeFunc(2, 1000)
+      ),
       1,
       scala.concurrent.ExecutionContext.global
     )
@@ -35,8 +44,8 @@ class FutureQueueTest extends FunSuite with ScalaFutures {
     assert(p1.isCompleted == false) // +250 ms (tot 250) the first promise should not be ready.
     assert(p1.isReadyWithin(Span(1500, Millis))) // +1500 ms (tot 1500) the first promise should be ready
     assert(p2.isCompleted == false) // +0 (tot 1500) the second promise should not be ready
-    assert(p2.isReadyWithin(Span(1500, Millis))) // +1000ms (tot 2500) the second promise should be ready
-    assert(queue.all().isReadyWithin(Span(100, Millis)))
+    assert(p2.isReadyWithin(Span(1000, Millis))) // +1000ms (tot 2500) the second promise should be ready
+    assert(queue.all().isCompleted)
   }
 
   test("Executes in parallel with concurrency above 1") {
@@ -51,9 +60,9 @@ class FutureQueueTest extends FunSuite with ScalaFutures {
     val p1 = queue.one(0)
     val p2 = queue.one(1)
 
-    assert(p1.isReadyWithin(Span(2000, Millis)))
+    assert(p1.isReadyWithin(Span(1500, Millis)))
     assert(p2.isCompleted)
-    assert(queue.all().isReadyWithin(Span(100, Millis)))
+    assert(queue.all().isCompleted)
   }
 
 }
